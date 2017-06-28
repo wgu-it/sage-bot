f743af0155b9a71a0d0df7c98c9efd03273fe8e1
Ryan Winchester <code@ryanwinchester.ca>
2017-06-28 13:28:07 -0700

    update what room

--- lib/sage/responders/debugger.ex
+++ lib/sage/responders/debugger.ex
@@ -3,6 +3,6 @@ defmodule Sage.Responders.Debugger do
   require Logger
 
   respond ~r/what room( is (this|it))?\??/i, msg do
-    reply msg, "We are in #{msg.room}."
+    reply msg, "We are in `#{msg.room}`."
   end
 end
