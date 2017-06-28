31a1bed6c7b0589cdbd0927d46f61544af990c04
Ryan Winchester <code@ryanwinchester.ca>
2017-06-27 19:14:07 -0700

    Initial commit

---
+++ lib/sage.ex
@@ -0,0 +1,18 @@
+defmodule Sage do
+  @moduledoc """
+  Documentation for Sage.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sage.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sage_test.exs
@@ -0,0 +1,8 @@
+defmodule SageTest do
+  use ExUnit.Case
+  doctest Sage
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
