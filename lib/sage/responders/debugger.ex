24aae7748c54dbcbc5e299ebc3849011ab489d57
Ryan Winchester <code@ryanwinchester.ca>
2017-06-28 13:52:06 -0700

    debug stuff

--- lib/sage/responders/debugger.ex
+++ lib/sage/responders/debugger.ex
@@ -2,6 +2,10 @@ defmodule Sage.Responders.Debugger do
   use Hedwig.Responder
   require Logger
 
+  respond ~r/debug (.+)/i, msg do
+    reply msg, "`#{msg.matches[1]}`"
+  end
+
   respond ~r/what room( is (this|it))?\??/i, msg do
     reply msg, "We are in `#{msg.room}`."
   end
