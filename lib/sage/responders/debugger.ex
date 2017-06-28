a4bff9562ce7c49f26df73c80da533aa2a2f5522
Ryan Winchester <code@ryanwinchester.ca>
2017-06-28 13:32:06 -0700

    who am i

--- lib/sage/responders/debugger.ex
+++ lib/sage/responders/debugger.ex
@@ -5,4 +5,8 @@ defmodule Sage.Responders.Debugger do
   respond ~r/what room( is (this|it))?\??/i, msg do
     reply msg, "We are in `#{msg.room}`."
   end
+
+  respond ~r/who am i\??/i, msg do
+    reply msg, "You are `#{msg.user}`."
+  end
 end
