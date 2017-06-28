6e38ded01c6d9617fc03cacb115b8ea0a1a8bedd
Ryan Winchester <code@ryanwinchester.ca>
2017-06-28 10:24:03 -0700

    update course describe regex fixes #3

--- lib/sage/responders/courses.ex
+++ lib/sage/responders/courses.ex
@@ -42,7 +42,7 @@ defmodule Sage.Responders.Courses do
   @usage """
   hedwig describe <code> - Responds with the course description
   """
-  respond ~r/describe (([a-zA-Z]{1,3})(?:\s|-)?([0-9]{1,4}))$/i, msg do
+  respond ~r/describe (([a-zA-Z]{1,3})(?:\s|-)?([0-9]{1,4}))/i, msg do
     code = sanitize(msg.matches[1])
 
     case CourseList.find_by_code(code) do
