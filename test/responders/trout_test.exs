b4a83f4ea46ea22eef255fa5434ffd0d505b60cb
Ryan Winchester <code@ryanwinchester.ca>
2017-07-05 20:50:36 -0700

    add to trout test

--- test/responders/trout_test.exs
+++ test/responders/trout_test.exs
@@ -8,4 +8,12 @@ defmodule Sage.Responders.TroutTest do
     assert_receive {:message, %{text: text}}, 500
     assert String.contains?(text, "slaps you around a bit with a large trout")
   end
+
+  @tag start_robot: true, name: "alfred", responders: [{Sage.Responders.Trout, []}]
+
+  test "slaps joe", %{adapter: adapter, msg: msg} do
+    send adapter, {:message, %{msg | text: "alfred slap joe"}}
+    assert_receive {:message, %{text: text}}, 500
+    assert String.contains?(text, "slaps joe around a bit with a large trout")
+  end
 end
