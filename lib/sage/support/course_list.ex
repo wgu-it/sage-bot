f0119d3de027d12e376b3068392b75c68abcbeda
CurtisLemansky <37763619+CurtisLemansky@users.noreply.github.com>
2018-03-24 21:50:19 -0700

    Add C851

--- lib/sage/support/course_list.ex
+++ lib/sage/support/course_list.ex
@@ -441,6 +441,10 @@ defmodule Sage.Support.CourseList do
       name: "Business of IT - Applications",
       desc: "Business of IT – Applications examines Information Technology Infrastructure Library (ITIL) terminology, structure, policies, and concepts. Focusing on the management of Information Technology (IT) infrastructure, development, and operations, students will explore the core principles of ITIL practices for service management to prepare them for careers as IT professionals, business managers, and business process owners. This course has no prerequisites."
     ],
+    "C851" => [
+      name: "Linux Foundations",
+      desc: "Linux Foundations is an introduction to Linux as an operating system as well as an introduction to open source concepts and the basics of the Linux command line. Expert content, a Linux virtual machine, and step-by-step labs give you hands-on access to practice Linux command line concepts. Linux is widely used in different industries for all kinds of functions including web servers, firewalls, and graphic design, and provides robust functionality and a stable, secure environment that is not often found in any other client operating system."
+    ],
     "C857" => [
       name: "Software Quality Assurance",
       desc: "Software Quality Assurance applies a QA focus to every phase of the software development life cycle. This course investigates best practices for quality analysis, quality planning, and testing strategies as they pertain to the everyday practice of software development. Students will come to understand how their work fits into the bigger picture: how QA, testing, and code-writing practices interact within specific process models; the potential impact of new code on existing code or on other applications; the importance of usability and the influence users have on the ultimate success of an application. Students will explore test plans, test cases, unit tests, integration tests, regression tests, usability tests, and test and review tools."
