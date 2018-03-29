5cae112dcbf5e2c975f29db09dec1decc32a621d
Liam Besaw <liam.besaw@gmail.com>
2018-03-29 00:25:02 -0700

    Added support for multiple matching course names in a message

--- lib/sage/responders/courses.ex
+++ lib/sage/responders/courses.ex
@@ -28,15 +28,8 @@ defmodule Sage.Responders.Courses do
   `<course code>` - Responds with the course code and name
   """
   hear ~r/(?:[a-zA-Z]{1,3})(?:\s|-)?(?:[0-9]{1,4})/i, msg do
-    code = sanitize(msg.matches[0])
-
-    case CourseList.find_by_code(code) do
-      {_code, course} ->
-        send msg, "*#{code}*: #{course[:name]}"
-      nil ->
-        Logger.warn "No match"
-        :ok
-    end
+    matches = Regex.scan(~r/(?:[a-zA-Z]{1,3})(?:\s|-)?(?:[0-9]{1,4})/i, msg.text)
+    Enum.each(matches, fn x-> send msg, compile(x) end)
   end
 
   @usage """
@@ -60,5 +53,16 @@ defmodule Sage.Responders.Courses do
     code
     |> String.replace(~r/[^a-zA-Z0-9]+/, "")
     |> String.upcase
+  end
+
+  def compile(match) do
+    match = sanitize(List.to_string(match))
+    case CourseList.find_by_code(match) do
+      {_code, course} ->
+        "*#{match}*: #{course[:name]}"
+      nil ->
+        Logger.warn "No match"
+        :ok
+    end
   end
 end
